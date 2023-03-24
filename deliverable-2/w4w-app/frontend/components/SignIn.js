912200cf0573ea2090f07c03695e6f20dd0ed950
notnine <g.jeremy@mail.utoronto.ca>
2023-03-23 23:23:56 -0400

    first 2 logos on welcome page
    Added 2 WFTW logos on the welcome page.
    Postions: top left, bottom center

--- deliverable-2/w4w-app/frontend/components/SignIn.js
+++ deliverable-2/w4w-app/frontend/components/SignIn.js
@@ -56,6 +56,8 @@ export default function SignIn({ navigation }) {
           <Icon name='angle-right' color='#03DAC5' size={15}/>
         </View>
       </Pressable>
+      <Image source={require('../assets/WFTW.png')}  style={{left: 0, top: 0, width: 110, height: 75, alignSelf: 'center'}}/>
+      <Image source={require('../assets/WFTW.png')}  style={{left: -150, bottom: 750, width: 75, height: 50, alignSelf: 'center'}}/>
     </View>
   );
 }
