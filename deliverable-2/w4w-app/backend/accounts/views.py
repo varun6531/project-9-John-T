ab30417b7f1f1cfae3f242ddd5e8069a7b9fcb2f
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2023-03-28 13:55:50 -0400

    Add comments for APIs

--- deliverable-2/w4w-app/backend/accounts/views.py
+++ deliverable-2/w4w-app/backend/accounts/views.py
@@ -6,6 +6,7 @@ from .models import PlayUser
 
 class RegistrationUser(APIView):
     def post(self, request):
+        """Registers a new default user."""
         serializers = UserSerializer(data=request.data)
         serializers.is_valid(raise_exception=True)
         serializers.save()
@@ -14,6 +15,7 @@ class RegistrationUser(APIView):
 
 class RegistrationStudent(APIView):
     def post(self, request):
+        """Registers a new student."""
         serializers = StudentSerializer(data=request.data)
         serializers.is_valid(raise_exception=True)
         serializers.save()
@@ -22,13 +24,16 @@ class RegistrationStudent(APIView):
 
 class RegistrationTeacher(APIView):
     def post(self, request):
+        """Registers a new teacher."""
         serializers = TeacherSerializer(data=request.data)
         serializers.is_valid(raise_exception=True)
         serializers.save()
         return Response(serializers.data)
 
+
 class UserType(APIView):
     def post(self, request):
+        """Returns the type of the user."""
         copy_data = request.data.copy()
         utype = PlayUser.objects.filter(email=copy_data["email"]).first().get_type()
         return Response({"type": utype})
@@ -36,6 +41,9 @@ class UserType(APIView):
 
 class UserHomeroom(APIView):
     def post(self, request):
+        """Returns the homeroom id of the user."""
         copy_data = request.data.copy()
-        homeroom_id = PlayUser.objects.filter(email=copy_data["email"]).first().get_homeroom()
+        homeroom_id = (
+            PlayUser.objects.filter(email=copy_data["email"]).first().get_homeroom()
+        )
         return Response({"homeroom_id": homeroom_id})
