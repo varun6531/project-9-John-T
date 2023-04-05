e53e3fe337246f53cc47272049332361069610ed
Nigel Yang <nigelyang611@hotmail.com>
2023-04-05 03:25:56 -0400

    Results page always lead to postQ

--- deliverable-2/w4w-app/frontend/components/Result80less.js
+++ deliverable-2/w4w-app/frontend/components/Result80less.js
@@ -46,7 +46,15 @@ export default function Result80less({ route, navigation }) {
                             Home
                         </Text>
                     </Pressable>
-                    <Pressable style={styles.button3} numberOfLines={1} onPress={async () => {
+                    <Pressable style={styles.button2} numberOfLines={1} onPress={() => navigation.navigate("Post questionnaire 1")}>
+                        <View style={styles.arrow2} >
+                            <FontAwesome5 name='angle-left' color='#03DAC5' size={15}/>
+                        </View>
+                        <Text style={styles.textButton}>
+                            Continue
+                        </Text>
+                    </Pressable>
+                    {/* <Pressable style={styles.button3} numberOfLines={1} onPress={async () => {
                         let user = await AsyncStorage.getItem("user")
                         if (user == null){
                             navigation.navigate("Tynl") 
@@ -65,7 +73,7 @@ export default function Result80less({ route, navigation }) {
                         <View style={styles.arrow} >
                         <FontAwesome5 name='angle-right' color='#03DAC5' size={15}/>
                         </View>
-                    </Pressable>
+                    </Pressable> */}
                 </View>  
             </View>
             <Image source={require('../assets/WFTW.png')} style={styles.w4twlogo}/>
